a779f956feecff414b6aece535c967859ff62743
Taro L. Saito <leo@xerial.org>
2024-12-18 21:33:27 +0000

    Update wvlet to 2024.9.17 based on 8bf037bfaa401c1c81fceff75bbb0e115d237724

--- wvlet.rb
+++ wvlet.rb
@@ -1,9 +1,9 @@
 class Wvlet < Formula
   desc "wvlet: A flow-style query language"
   homepage "https://wvlet.org/wvlet"
-  url "https://github.com/wvlet/wvlet/releases/download/v2024.9.16/wvlet-cli-2024.9.16.tar.gz"
-  version "2024.9.16"
-  sha256 "1dc6aabae11532fcaf8be42f21fc59f06c7528fa0115d5f8c9b1c65939be5a8a"
+  url "https://github.com/wvlet/wvlet/releases/download/v2024.9.17/wvlet-cli-2024.9.17.tar.gz"
+  version "2024.9.17"
+  sha256 "e8a4d7d7a1500a17ea2f1be3ff47bcac219e7d32b3bc1e9baba780c3f8cb4a49"
   license "Apache-2.0"
 
   depends_on "openjdk@21"
